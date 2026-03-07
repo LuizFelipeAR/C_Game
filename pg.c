6afd0ba86e76b047e43bbecf854f5568dd8ab34f
luizfelipear <luizfelipearaujoreis5@gmail.com>
2026-03-06 22:45:16 -0300

    teste contador

---
+++ pg.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+
+int main() {
+    time_t inicio;
+    time_t final;
+    double diferenca;
+
+    printf(" __\n");
+    printf("/_ |\n");
+    printf(" | |\n");
+    printf(" | |\n");
+    printf(" | |\n");
+    printf(" |_|\n");
+
+    time(&inicio);
+
+    while(diferenca < 1.00000) {
+        time(&final);
+        diferenca = difftime(final, inicio);
+    }
+
+    if(diferenca == 1.00000) {
+        printf(" ____\n");
+        printf("|__  | \n");
+        printf("   ) |\n");
+        printf("  / / \n");
+        printf(" / /_ \n");
+        printf("|____|\n");
+    }
+
+       while(diferenca < 2.00000) {
+        time(&final);
+        diferenca = difftime(final, inicio);
+    }
+    return 0;
+}
