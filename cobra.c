bd1877a4b07283522e3ba807352c4d10c2f18359
Pedro Souza <pedroarthurfsouza@gmail.com>
2026-03-10 11:47:16 -0300

    nomes a serem selecionados

--- cobra.c
+++ cobra.c
@@ -5,10 +5,10 @@ printf("Cobras na caixa!!\n\n");
 printf("Olá exploradores! Parece que voces estao presos numa tumba egipicia!\n");
 printf("em frente voces verao cinco caixas, numa delas ha um botao para abrirem a porta e sairem da tumba...\n");
 printf("Em outra caixa ha uma cobra que vai matar voces!!!!!!\n");
-printf("mas nas outras caias nao tem nada nao =D\nBoa Sorte!");
+printf("mas nas outras caias nao tem nada nao =D\nBoa Sorte!\n\n");
 
-printf("mimimi escolha um dos sete nomes jogador 1 blablala");
-
+printf("Jogador 1, Escolha um nome:\n");
+printf("1) Alan\n2) Linus\n3) Steve\n4) Ada\n5) Neumann\n6) Zé Valim\n7) Rob Ieru\n\nDigite o número correspondente ao nome escolhido.");
 
     return 0;
 }
