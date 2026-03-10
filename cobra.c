430187a3eeaf8e3c47694bbdd25df8d24ee0af42
luizfelipear <luizfelipearaujoreis5@gmail.com>
2026-03-10 15:57:43 -0300

    M

--- cobra.c
+++ cobra.c
@@ -5,7 +5,7 @@ printf("Cobras na caixa!!\n\n");
 printf("Olá exploradores! Parece que voces estao presos numa tumba egipicia!\n");
 printf("em frente voces verao cinco caixas, numa delas ha um botao para abrirem a porta e sairem da tumba...\n");
 printf("Em outra caixa ha uma cobra que vai MATAR voces!!!!!!\n");
-printf("mas nas outras caias nao tem nada nao =D\nBoa Sorte!\n\n");
+printf("Mas nas outras caias nao tem nada nao =D\nBoa Sorte!\n\n");
 
 printf("Jogador 1, Escolha um nome:\n");
 printf("1) Alan\n2) Linus\n3) Steve\n4) Ada\n5) Neumann\n6) Zé Valim\n7) Rob Ieru\n\nDigite o número correspondente ao nome escolhido.");
