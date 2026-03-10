587cc7941c8877adb21635caed83bb40b5c0062f
Pedro Arthur Souza <pedroarthurfsouza@gmail.com>
2026-03-10 15:50:56 -0300

    Atualizar o cobra.c

--- cobra.c
+++ cobra.c
@@ -4,7 +4,7 @@ int main () {
 printf("Cobras na caixa!!\n\n");
 printf("Olá exploradores! Parece que voces estao presos numa tumba egipicia!\n");
 printf("em frente voces verao cinco caixas, numa delas ha um botao para abrirem a porta e sairem da tumba...\n");
-printf("Em outra caixa ha uma cobra que vai matar voces!!!!!!\n");
+printf("Em outra caixa ha uma cobra que vai MATAR voces!!!!!!\n");
 printf("mas nas outras caias nao tem nada nao =D\nBoa Sorte!\n\n");
 
 printf("Jogador 1, Escolha um nome:\n");
