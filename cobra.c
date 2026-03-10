3db3f2f548973a663f23969db0949c6148d3bb26
Pedro Souza <pedroarthurfsouza@gmail.com>
2026-03-10 01:08:02 -0300

    inicio do dialogo texto do jogo dois

---
+++ cobra.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main () {
+printf("Cobras na caixa!!\n\n");
+printf("Olá exploradores! Parece que voces estao presos numa tumba egipicia!\n");
+printf("em frente voces verao cinco caixas, numa delas ha um botao para abrirem a porta e sairem da tumba...\n");
+printf("Em outra caixa ha uma cobra que vai matar voces!!!!!!\n");
+printf("mas nas outras caias nao tem nada nao =D\nBoa Sorte!");
+
+printf("mimimi escolha um dos sete nomes jogador 1 blablala");
+
+
+    return 0;
+}
