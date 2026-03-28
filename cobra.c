57d95a19f5ad696b2e452cf0ab3e5e25a7185b78
Pedro Arthur Souza <pedroarthurfsouza@gmail.com>
2026-03-28 19:52:53 -0300

    mudei tudo de novo, agora falta só ciclar tudo e fazer menuzin de voltar pro inicio do jogo ou sair do jogo

--- cobra.c
+++ cobra.c
@@ -1,9 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
+#include <unistd.h>
 
 int main(int argc, char *argv[]) {
 
+srand(time(NULL));
+
+/*
 sleep(1);
 printf("Cobras na caixa!!\n\n");
 sleep(2);
@@ -11,10 +16,11 @@ printf("Ola exploradores! Parece que voces estao presos numa tumba egipicia!\n")
 sleep(2);
 printf("em frente voces verao cinco caixas, numa delas ha um botao para abrirem a porta e sairem da tumba...\n");
 sleep(4);
-printf("Em outra caixa ha uma cobra que vai MATAR voces!!!!!!\n");
+printf("Em uma outra caixa ha uma cobra que vai MATAR voces!!!!!!\n");
 sleep(2);
 printf("Mas nas outras caixas nao tem nada nao =D\nBoa Sorte!\n\n");
 sleep(2);
+*/
 
 printf("\n\nJogador 1, Escolha um nome:\n");
 printf("1) Alan\n2) Linus\n3) Steve\n4) Ada\n5) Neumann\n6) Ze-Valim\n7) Rob-Ieru\n\nDigite o numero correspondente ao nome escolhido:\n");
@@ -64,11 +70,139 @@ scanf("%d", &pick2);
 strcpy(jogador2, nomes[pick2 - 1]);
 
 
+int caixas[5] = {0,0,0,0,0}; //array de ints, com 5 caixas (0 ate 4)
+int caixacobra = rand() %5; //randomiza um numero de 0 a 4 dentro de um outro int
+caixas[caixacobra] = 1; //o numero randomizado vai indicar qual das caixas dentro do array vai ser escolhida pra ter 1 como valor (representando a cobra)
+
+int caixabotao; 
+do{
+caixabotao = rand() %5; //randomiza o numero de uma outra variavel
+}while(caixabotao == caixacobra);//enquanto o valor da caixa do botao for o mesmo do valor da caixa da cobra ele vai continuar randomizando esse numero
+
+caixas[caixabotao] = 2;
+//aqui eu escolhi randomicamente duas caixas, uma com valor 1 e outra com valor 2, o resto eh zero pq nao tem nada dentro.
+
+//cada elemento (o nada, a cobra e o botao) eh representado por um numero, o 0 eh nada, a cobra eh 1 e o botao eh b
 
 
+printf("\nVamos %s e %s, voces precisam sair da tumba!\n", jogador1,jogador2);
+sleep(2);
+
+int turno = rand() %2; //randomiza entre 0 e 1 a vez dos jogadores
 
 
+if(turno==0){
+	
+	system("cls||clear");
+	printf("Agora eh a vez de %s\nESCOLHA UMA DAS CAIXAS\n\n", jogador1);
+	
+	printf("+---+ +---+ +---+ +---+ +---+\n");
+	printf("| 1 | | 2 | | 3 | | 4 | | 5 |\n");
+	printf("+---+ +---+ +---+ +---+ +---+\n");
 
+	sleep(1);
+	system("cls||clear");
+
+	printf("Agora eh a vez de %s\nESCOLHA UMA DAS CAIXAS\n\n", jogador1);
+	printf("+---+ +---+ +---+ +---+ +---+\n");
+	printf("| 5 | | 1 | | 2 | | 3 | | 4 |\n");
+	printf("+---+ +---+ +---+ +---+ +---+\n");
+
+	sleep(1);
+	system("cls||clear");
+	
+	printf("Agora eh a vez de %s\nESCOLHA UMA DAS CAIXAS\n\n", jogador1);
+	printf("+---+ +---+ +---+ +---+ +---+\n");
+	printf("| 4 | | 5 | | 1 | | 2 | | 3 |\n");
+	printf("+---+ +---+ +---+ +---+ +---+\n");
+
+	sleep(1);
+	system("cls||clear");
+
+	printf("Agora eh a vez de %s\nESCOLHA UMA DAS CAIXAS\n\n", jogador1);
+	printf("+---+ +---+ +---+ +---+ +---+\n");
+	printf("| 3 | | 4 | | 5 | | 1 | | 2 |\n");
+	printf("+---+ +---+ +---+ +---+ +---+\n");
+
+	sleep(1);
+	system("cls||clear");
+
+	printf("Agora eh a vez de %s\nESCOLHA UMA DAS CAIXAS\n\n", jogador1);
+	printf("+---+ +---+ +---+ +---+ +---+\n");
+	printf("| 2 | | 3 | | 4 | | 5 | | 1 |\n");
+	printf("+---+ +---+ +---+ +---+ +---+\n");
+
+	sleep(1);
+	system("cls||clear");
+
+	printf("Agora eh a vez de %s\nESCOLHA UMA DAS CAIXAS\n\n", jogador1);	
+	printf("+---+ +---+ +---+ +---+ +---+\n");
+	printf("| 1 | | 2 | | 3 | | 4 | | 5 |\n");
+	printf("+---+ +---+ +---+ +---+ +---+\n");
+	
+
+} else {
+	
+	system("cls||clear");
+	printf("Agora eh a vez de %s\nESCOLHA UMA DAS CAIXAS\n\n", jogador2);
+	
+	printf("+---+ +---+ +---+ +---+ +---+\n");
+	printf("| 1 | | 2 | | 3 | | 4 | | 5 |\n");
+	printf("+---+ +---+ +---+ +---+ +---+\n");
+
+	sleep(1);
+	system("cls||clear");
+
+	printf("Agora eh a vez de %s\nESCOLHA UMA DAS CAIXAS\n\n", jogador2);
+	printf("+---+ +---+ +---+ +---+ +---+\n");
+	printf("| 5 | | 1 | | 2 | | 3 | | 4 |\n");
+	printf("+---+ +---+ +---+ +---+ +---+\n");
+
+	sleep(1);
+	system("cls||clear");
+
+	printf("Agora eh a vez de %s\nESCOLHA UMA DAS CAIXAS\n\n", jogador2);	
+	printf("+---+ +---+ +---+ +---+ +---+\n");
+	printf("| 4 | | 5 | | 1 | | 2 | | 3 |\n");
+	printf("+---+ +---+ +---+ +---+ +---+\n");
+
+	sleep(1);
+	system("cls||clear");
+
+	printf("Agora eh a vez de %s\nESCOLHA UMA DAS CAIXAS\n\n", jogador2);
+	printf("+---+ +---+ +---+ +---+ +---+\n");
+	printf("| 3 | | 4 | | 5 | | 1 | | 2 |\n");
+	printf("+---+ +---+ +---+ +---+ +---+\n");
+
+	sleep(1);
+	system("cls||clear");
+
+	printf("Agora eh a vez de %s\nESCOLHA UMA DAS CAIXAS\n\n", jogador2);
+	printf("+---+ +---+ +---+ +---+ +---+\n");
+	printf("| 2 | | 3 | | 4 | | 5 | | 1 |\n");
+	printf("+---+ +---+ +---+ +---+ +---+\n");
+
+	sleep(1);
+	system("cls||clear");
+
+	printf("Agora eh a vez de %s\nESCOLHA UMA DAS CAIXAS\n\n", jogador2);	
+	printf("+---+ +---+ +---+ +---+ +---+\n");
+	printf("| 1 | | 2 | | 3 | | 4 | | 5 |\n");
+	printf("+---+ +---+ +---+ +---+ +---+\n");
+	
+}
+
+int escolhacaixa;
+printf("escolha uma das caixas digitando o numero correspondente a ela!!");
+scanf("%d", &escolhacaixa);
+
+	if((escolhacaixa-1)==caixabotao){
+	printf("parabens jogadores!voces venceram");
+	} else if((escolhacaixa-1)==caixacobra){
+	printf("ahhh naooo, parece que voces escolheram a cobra e MORRERAM =(");
+	} else {
+	printf("opa, parece que esta caixa esta vazia =/");
+	}
 
 
 
