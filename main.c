d49c56ef509cb6d89ff2d5f6f4d3effdcbf05ba9
Pedro Arthur Souza <pedroarthurfsouza@gmail.com>
2026-04-05 22:31:30 -0300

    adicionei o cobras na caixa no menu

--- main.c
+++ main.c
@@ -1,52 +1,33 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void CobrasNaCaixa();
 
 int main(int argc, char *argv[]) {
-	
-	system("color 3");
-	int esc;
-	
-	printf(" GCPLAY\n\n");
-	
-	printf(" 1: Pergunta e Resposta\n");
-	printf(" 2: Cobra na Caixa!\n");
-	printf(" 3: Gousmas War\n");
-	printf(" 4: Sair\n\n");
-	
-	scanf("%d",&esc);
-	
-	
-	if (esc == 1){
-	
-		system("cls");
-		printf("\t\t Pergunta e Resposta");
-	
-	}
-	
-	if (esc == 2){
-	
-		system("cls");
-		printf("\t\t Cobra na Caixa!");
-	
-	}
 
-	if (esc == 3){
-	
-		system("cls");
-		printf("\t\t Gousmas War");
-	
-	}
+    while(1){
+	system("cls");
+        system("color 3");
+        int esc;
 
-	if (esc == 4){
-	
-		exit(EXIT_SUCCESS);
-	}	
-	
-	
-	
-	
-	
-	
-	return 0;
+        printf(" GCPLAY\n\n");
+
+        printf(" 1: Pergunta e Resposta\n");
+        printf(" 2: Cobra na Caixa!\n");
+        printf(" 3: Gousmas War\n");
+        printf(" 4: Sair\n\n");
+
+        scanf("%d",&esc);
+
+        if (esc == 2){
+            system("cls");
+            CobrasNaCaixa();
+        }
+
+        if (esc == 4){
+            exit(EXIT_SUCCESS);
+        }
+    }
+
+    return 0;
 }
